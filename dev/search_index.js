1e98d3a79c1810d4546935daf5f7093ed78e3d1a
Documenter.jl <documenter@juliadocs.github.io>
2022-06-21 04:07:19 +0000

    build based on e3f20f7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OCR","category":"page"},{"location":"#OCR","page":"Home","title":"OCR","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OCR.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OCR]","category":"page"}]
+}
