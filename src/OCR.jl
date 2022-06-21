6a006f8f0edc3c1e6c398763751a17d5e19f3ef4
Lilith Hafner <Lilith.Hafner@gmail.com>
2022-06-20 20:25:07 -0400

    basic training data

--- src/OCR.jl
+++ src/OCR.jl
@@ -1,5 +1,24 @@
 module OCR
 
-# Write your package code here.
+using Images, BasicTextRender
+
+export training_data
+
+function training_data(n=512, m=500, textsize=10)
+    img = rand(Gray{Float64},n,n) ./ 2 .+ .5
+
+    chars = vcat('a':'z', 'A':'Z', '0':'9')
+
+    locs = []
+    n0 = n - ceil(Int, ℯ * textsize)
+    for _ in 1:m
+        xy = rand(1:n0), rand(1:n0)
+        _textsize = round(Int, textsize*exp(rand()))
+        _textsize == 23 && continue # https://github.com/IanButterworth/BasicTextRender.jl/issues/2
+        overlay = overlaytext!(img, string(rand(chars)), _textsize, xy)
+        push!(locs, xy .+ size(overlay) ./ 2)
+    end
+    img    
+end
 
 end
