e3f20f7afa7169eb1371a372195adf9d281ce9aa
Lilith Hafner <Lilith.Hafner@gmail.com>
2022-06-21 00:02:50 -0400

    Train the network on the data and post-process to recognize characters

--- src/OCR.jl
+++ src/OCR.jl
@@ -1,6 +1,6 @@
 module OCR
 
-using Images, BasicTextRender, StatsBase
+using Images, BasicTextRender, StatsBase, Random
 
 export training_data, overlay
 
@@ -93,14 +93,110 @@ end
 
 function test_network()
     input = rand(17, 5)
-    target = rand(17, 5)
-    network = weights([5, 15, 15, 5])
+    target = rand(17, 4)
+    network = weights([5, 15, 15, 4])
     for i in 1:10000
         prediction, history = forward(network, input)
         grad = .0001(target .- prediction)
         isinteger(log2(i)) && println(mean((target .- prediction).^2))
         backward!(network, history, grad)
     end
+end
+
+margin(source, x, y) = source[begin+x:end-x, begin+y:end-y]
+
+function epoch(network; coords, img, dist, window, train, window_length, batch_size)
+    loss = []
+    live = []
+    for i in firstindex(coords):batch_size:lastindex(coords)
+        bs = min(batch_size, lastindex(coords)-i+1)
+        input = Matrix{Float64}(undef, bs, window_length)
+        target = Matrix{Float64}(undef, bs, 1)
+        for (j, loc) in enumerate(coords[i:i+bs-1])
+            input[j, :] = vec(img[ntuple(i->loc[i]-window[i]:loc[i]+window[i], 2)...])
+            target[j, 1] = 1/(2+dist[loc])
+        end
+
+        prediction, history = forward(network, input)
+        push!(loss, mean((target .- prediction).^2))
+        push!(live, mean(hcat(history...) .> 0))
+
+        if train
+            grad = 1e-3(target .- prediction)
+            backward!(network, history, grad)
+        end
+    end
+    mean(loss), mean(live)
+end
+
+function train(epochs)
+    window = (7,4)
+    window_length = prod(window .* 2 .+ 1)
+
+    v_img, _, v_dist = training_data(256)
+
+    _t = (1 ./ (2 .+ v_dist))
+    println("0 baseline: $(mean(_t .^ 2))")
+    println("const baseline: $(var(_t))")
+
+    v_coords = vec(margin(eachindex(IndexCartesian(), v_img), window...))
+
+    network = weights([window_length, 200, 100, 50, 10, 1]) * .1
+    batch_size = 200
+
+    for _ in 1:epochs
+        
+        print(epoch(network; coords=v_coords, img=v_img, dist=v_dist, window, batch_size=3batch_size, window_length, train=false))
+
+        print('\t')
+
+        img, _, dist = training_data(256)
+        coords = shuffle(vec(margin(eachindex(IndexCartesian(), v_img), window...)))
+        println(epoch(network; coords, img, dist, window, batch_size, window_length, train=true))
+
+    end
+
+    v_img, apply(network; img=v_img, window, window_length, batch_size)
+end
+
+function apply(network; img, window, window_length, batch_size)
+    out = zeros(size(img)...)
+    coords = margin(eachindex(IndexCartesian(), img), window...)
+    for i in firstindex(coords):batch_size:lastindex(coords)
+        bs = min(batch_size, lastindex(coords)-i+1)
+        input = Matrix{Float64}(undef, bs, window_length)
+        for (j, loc) in enumerate(coords[i:i+bs-1])
+            input[j, :] = vec(img[ntuple(i->loc[i]-window[i]:loc[i]+window[i], 2)...])
+        end
+        prediction, _ = forward(network, input)
+        out[coords[i:i+bs-1]] .= vec(prediction)
+    end
+
+    out
+end
+
+function blur(img)
+    a = 2 * img
+    a[begin+1:end, :] += img[begin:end-1, :]
+    a[begin:end-1, :] += img[begin+1:end, :]
+    out = 2 * a
+    out[:, begin+1:end] += a[:, begin:end-1]
+    out[:, begin:end-1] += a[:, begin+1:end]
+    out ./= 16
+end
+
+function peaks(img)
+    out = img .> quantile(vec(img), .8)
+    out[begin+1:end, :] .&= img[begin+1:end, :] .>= img[begin:end-1, :]
+    out[begin:end-1, :] .&= img[begin:end-1, :] .> img[begin+1:end, :]
+    out[:, begin+1:end] .&= img[:, begin+1:end] .>= img[:, begin:end-1]
+    out[:, begin:end-1] .&= img[:, begin:end-1] .> img[:, begin+1:end]
+    out
+end
+
+function test_train(epochs)
+    img, output = train(epochs)
+    overlay(img, peaks(blur(blur(output))))
 end
 
 end # module
